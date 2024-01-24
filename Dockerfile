ae979dd2accd43af159cb323c84632580263f784
Vladislav Nesterov <darveivoldavara@gmail.com>
2024-01-24 20:42:40 +0300

    Add performance coefficient script

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM pytorch/pytorch:2.1.0-cuda12.1-cudnn8-devel
+
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y git
+RUN pip install git+https://github.com/linto-ai/whisper-timestamped.git#egg=whisper-timestamped[dev,vad_silero,test]
+
+COPY requirements.txt /app
+RUN pip install -r requirements.txt
+
+COPY transcribe.py parse.py /app/
+
+CMD ["python", "transcribe.py"]
--- transcribe.py
+++ transcribe.py
@@ -2,30 +2,55 @@ import whisper_timestamped as whisper
 import json
 import os
 import torch
+import time
+import wave
 from parse import convert_json_to_text
+from statistics import median
+
+
+def get_audio_duration(wav_filename):
+    with wave.open(wav_filename, "r") as wav_file:
+        frames = wav_file.getnframes()
+        rate = wav_file.getframerate()
+        duration = frames / float(rate)
+        return duration
+
 
 def main():
-    
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
-    model = whisper.load_model("large-v3", device=device, download_root='./cache')
+
+    model = whisper.load_model("large-v3", device=device, download_root="./cache")
     print("Model loaded successfully")
-    
+
+    performance_ratios = []
+
     # Iterate files in data
     for filename in sorted(os.listdir("input")):
         print(f"Transcribing {filename}")
-        audio = whisper.load_audio(f"input/{filename}")
-        # https://github.com/linto-ai/whisper-timestamped/blob/master/whisper_timestamped/transcribe.py
-        result = whisper.transcribe(
-            model, 
-            audio, 
-            language="ru"            
-            )
+
+        audio_path = f"input/{filename}"
+        audio = whisper.load_audio(audio_path)
+        start_time = time.time()
+        result = whisper.transcribe(model, audio, vad=True, language="ru")
+        duration = time.time() - start_time
+
         with open(f"output/{filename}.json", "w") as f:
             json.dump(result, f, indent=2, ensure_ascii=False)
-        print(f"Transcribed {filename}")
-    
+
+        audio_duration = get_audio_duration(audio_path)
+        performance_ratio = duration / audio_duration
+        performance_ratios.append(performance_ratio)
+
+        print(
+            f"Transcribed {filename} in {duration} seconds with performance ratio: {performance_ratio}"
+        )
+
+    median_performance_ratio = median(performance_ratios)
+    print(f"Median Performance Ratio: {median_performance_ratio}")
+
     convert_json_to_text()
+
 
 if __name__ == "__main__":
     main()
