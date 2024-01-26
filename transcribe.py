f19d4bf4e7eeb443138d04f23c100cb56f03eaff
Vladislav Nesterov <darveivoldavara@gmail.com>
2024-01-26 13:22:37 +0300

    modified: adeed error handling, optimized logging

--- transcribe.py
+++ transcribe.py
@@ -32,8 +32,11 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     logger.info(f"Using device: {device}")
 
-    model = whisper.load_model("large-v3", device=device, download_root="./cache")
-    logger.info("Model loaded successfully")
+    try:
+        model = whisper.load_model("large-v3", device=device, download_root="./cache")
+        logger.info("Model loaded successfully")
+    except Exception as exc:
+        raise exc
 
     performance_ratios = []
     results = []
@@ -44,33 +47,33 @@ def main():
         if not "wav" in filename:
             logger.warning(f"Skipping {filename}")
             continue
-        logger.info(f"Transcribing {filename}")
 
         audio_path = f"input/{filename}"
         audio = whisper.load_audio(audio_path)
-        start_time = time.time()
-        result = whisper.transcribe(model, audio, vad=True, language="ru")
-        duration_transcript = time.time() - start_time
 
-        with open(f"output/{filename}.json", "w") as f:
-            json.dump(result, f, indent=2, ensure_ascii=False)
+        try:
+            start_time = time.time()
+            result = whisper.transcribe(model, audio, vad=True, language="ru")
+            duration_transcript = time.time() - start_time
 
-        duration_audio = get_audio_duration(audio_path)
-        performance_ratio = duration_transcript / duration_audio
-        performance_ratios.append(performance_ratio)
+            with open(f"output/{filename}.json", "w") as f:
+                json.dump(result, f, indent=2, ensure_ascii=False)
 
-        results.append(
-            {
-                "filename": filename,
-                "duration_audio": duration_audio,
-                "duration_transcript": duration_transcript,
-                "performance_ratio": performance_ratio,
-            }
-        )
+            duration_audio = get_audio_duration(audio_path)
+            performance_ratio = duration_transcript / duration_audio
+            performance_ratios.append(performance_ratio)
 
-        logger.info(
-            f"Transcribed {filename} in {duration_transcript} seconds with performance ratio: {performance_ratio}"
-        )
+            results.append(
+                {
+                    "filename": filename,
+                    "duration_audio": duration_audio,
+                    "duration_transcript": duration_transcript,
+                    "performance_ratio": performance_ratio,
+                }
+            )
+        except Exception as e:
+            logger.error(f"{e}")
+            continue
 
     median_performance_ratio = median(performance_ratios)
     logger.info(f"Median Performance Ratio: {median_performance_ratio}")
